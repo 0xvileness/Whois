41d577141db549a6f4cfa0c7f155ad4e14730791
0xvileness <thatonedude9773@gmail.com>
2025-05-29 16:11:58 -0400

    Create whois.py

---
+++ whois.py
@@ -0,0 +1,20 @@
+import socket
+import ipwhois import IPWhois
+from pprint import ppring 
+
+print()
+print("  ╦ ╦┬ ┬┌─┐┬┌─┐ ")
+print("  ║║║├─┤│ ││└─┐ ")
+print("  ╚╩╝┴ ┴└─┘┴└─┘ ")
+print(" Made By @Oxycrime ")
+print()
+
+target = input("Enter Website")
+
+#get ip of the server
+ip = socket.gethostbyname(target)
+obj = IPWhois(ip)
+obj = obj.lookup_whois()
+
+#out put 
+pprint(obj)
